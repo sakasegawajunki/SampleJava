ab1b1b483eb3abefdd06001305382cffd9374a26
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-09-16 20:14:25 +0900

    セクション2

---
+++ SampleProject/src/jp/learningdesign/javastudy/Sample.java
@@ -0,0 +1,12 @@
+package jp.learningdesign.javastudy;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello Eclipse!");
+		System.out.println("Hello World!");
+		
+	}
+
+}
